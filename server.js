9a30a76c74f7bdd98e059420431ce8c9cd722ae2
Glitch (fcc-file-metadata-proj) <none>
2019-06-07 04:57:36 +0000

    🙏🌎 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+const bodyParser = require('body-parser');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
+
+// require and use "multer"...
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req,res,next){
+  // console.log(req.file);
+  res.json({name: req.file.originalname, type: req.file.mimetype, filesize: req.file.size});
+  
+});
+
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
